be27bce2f8783d6a3cbf079064807e01183fcaf0
Juraj Veverka <gergej123@gmail.com>
2023-12-04 23:40:02 +0100

    adding more stats

--- ev-statistics/ev-stats.py
+++ ev-statistics/ev-stats.py
@@ -30,6 +30,10 @@ with open(file_name, newline='') as csvfile:
     battery_temp_counter = 0
 
     battery_soh = 0
+    battery_soc = 0
+    battery_cap = 0
+    battery_cap_calculated = 0
+    range_calculated = 0
 
     for row in reader:
         
@@ -40,6 +44,8 @@ with open(file_name, newline='') as csvfile:
         ambient_temp_string = row['Ambient temperature (℃)']
         battery_temp_string = row['DC Battery Temperature (℃)']
         battery_soh_string = row['State of health (%)']
+        battery_soc_string = row['State of charge (%)']
+        battery_cap_string = row['Battery DC Capacity (kWh)']
         df = datetime.strptime(timestring,'%H:%M:%S.%f')
         seconds = float(df.hour*3600) + float(df.minute*60) + float(df.second) + float(df.microsecond)/1000000
         if not start_time:
@@ -87,11 +93,24 @@ with open(file_name, newline='') as csvfile:
         if battery_soh_string:
            battery_soh = float(battery_soh_string)
 
+        if battery_soc_string:
+           battery_soc = float(battery_soc_string)
+
+        if battery_cap_string:
+           battery_cap = float(battery_cap_string)
+
+        if battery_soc > 0 and battery_cap > 0 :
+           battery_cap_calculated_new = ( battery_cap / battery_soc ) * 100   
+           if battery_cap_calculated_new > battery_cap_calculated:
+              battery_cap_calculated = battery_cap_calculated_new
+
+
 time_interval = end_time - start_time
 average_speed = speed_sum / speed_counter
 average_consumption = power_summary / (distance_total/100)
 avg_amb_temperature = ambient_temp_sum / ambient_temp_counter 
 ave_bat_temperature = battery_temp_sum / battery_temp_counter 
+max_range_estimated = ( battery_cap_calculated / average_consumption ) * 100
 
 print("")
 print("### Trip:", file_name)
@@ -105,6 +124,8 @@ print("| avg. consumtion | ", round(average_consumption, 2), "kWh/100km", "  |")
 print("| avg. amb. temp. | ", round(avg_amb_temperature, 2), "℃", "  |")
 print("| avg. bat. temp. | ", round(ave_bat_temperature, 2), "℃", "  |")
 print("| battery SoH     | ", round(battery_soh, 2), "%", "  |")
+print("| max battery cap.| ", round(battery_cap_calculated, 2), "kWh", "  |")
+print("| max range estim.| ", round(max_range_estimated, 2), "km", "  |")
 print("")
 
 
